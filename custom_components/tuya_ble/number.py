a2a0d82d11c52cdf84d367a9531da457f8d11ac6
Pascal Gödeke <pascal.goedeke@outlook.de>
2025-11-25 15:22:01 +0100

    Update number.py

--- custom_components/tuya_ble/number.py
+++ custom_components/tuya_ble/number.py
@@ -355,7 +355,8 @@ mapping: dict[str, TuyaBLECategoryNumberMapping] = {
             **dict.fromkeys(
                 [
                     "mknd4lci",
-                    "riecov42"
+                    "riecov42",
+                    "bs3ubslo"
                 ],  # Fingerbot Plus
                 [
                     TuyaBLENumberMapping(
